1a491191b080a75ee32712e37b103b57f98783a3
shubhechchhakarmacharya <shubhechchhakarmacharya@gmail.com>
2023-04-27 21:20:17 +0545

    School management System

---
+++ footer.php
@@ -0,0 +1,8 @@
+ <footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      Montessori Management System
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2020.</strong> Alisha Khadka.
+  </footer>
